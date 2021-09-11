508924448a7f1733d8778c637e8df97dd1a12d02
Варвара Агапова <agaperra@yandex.ru>
2021-09-11 11:46:04 +0300

    lab №1

--- main.py
+++ main.py
@@ -1,3 +1,108 @@
+import numpy
+from PIL import Image
+import math
+import matplotlib.pyplot as pyplot
+import random
+from sklearn.model_selection import GridSearchCV
+from sklearn.neural_network import MLPClassifier
+
 # Лабораторная работа №1 по курсу Цифровые технологии обработки информации
 
-print("Hello, darling")
+# print("Hello, darling")
+
+# responses[i,j]=math.exp(l1*t)*math.sin(l2*t) колебательный
+# responses[i,j]=math.exp(l1*t)-math.exp(l2*t) апериодический
+# l1 = -(w0)/(Q*2) колебательный
+# l2 = D/2
+# l1 = (-w0/(2Q)+D/2) апериодический
+# l2 = (-w0/(2Q)-D/2)
+# t = j*dt
+# D= math.sqrt(w0*w0*(1/(Q*Q) -4))
+
+responses = numpy.zeros((50, 100))
+# print(responses)
+response_type = numpy.zeros(50, int)
+# print(response_type)
+
+w0 = 0.1
+dt = 0.1 / w0
+for i in range(0, 50):
+    Q = random.random()
+    if Q < 0.5:
+        # апериодический
+        D = math.sqrt(w0 * w0 * (1 / (Q * Q) - 4))
+        l1 = (-w0 / (2 * Q) + D / 2)
+        l2 = (-w0 / (2 * Q) - D / 2)
+    else:
+        if Q > 0.5:
+            # колебательный
+            D = math.sqrt(w0 * w0 * (4 - 1 / (Q * Q)))
+            l1 = -w0 / (Q * 2)
+            l2 = D / 2
+
+    for j in range(0, 100):
+        t = j * dt
+        if Q < 0.5:
+            # апериодический
+            responses[i, j] = math.exp(l1 * t) - math.exp(l2 * t)
+            response_type[i] = -1
+        else:
+            if Q > 0.5:
+                # колебательный
+                responses[i, j] = math.exp(l1 * t) * math.sin(l2 * t)
+                response_type[i] = 1
+
+print(response_type)
+# pyplot.plot(responses[10, :], 'o')
+# pyplot.show()
+
+param_grid = {'max_iter': [1000], 'activation': ['tanh'], 'alpha': [0.65],
+              'hidden_layer_sizes': [1], 'solver': ['lbfgs']}
+grid_search = GridSearchCV(MLPClassifier(), param_grid, cv=2)
+grid_search.fit(responses, response_type)
+
+responses_test = numpy.zeros((100, 100))
+# print(responses)
+response_type_test = numpy.zeros(100, int)
+# print(response_type)
+
+w0 = 0.1
+dt = 0.1 / w0
+for i in range(0, 100):
+    Q = random.random()
+    if Q < 0.5:
+        # апериодический
+        D = math.sqrt(w0 * w0 * (1 / (Q * Q) - 4))
+        l1 = (-w0 / (2 * Q) + D / 2)
+        l2 = (-w0 / (2 * Q) - D / 2)
+    else:
+        if Q > 0.5:
+            # колебательный
+            D = math.sqrt(w0 * w0 * (4 - 1 / (Q * Q)))
+            l1 = -w0 / (Q * 2)
+            l2 = D / 2
+
+    for j in range(0, 100):
+        t = j * dt
+        if Q < 0.5:
+            # апериодический
+            responses_test[i, j] = math.exp(l1 * t) - math.exp(l2 * t)
+            response_type_test[i] = -1
+        else:
+            if Q > 0.5:
+                # колебательный
+                responses_test[i, j] = math.exp(l1 * t) * math.sin(l2 * t)
+                response_type_test[i] = 1
+
+store = grid_search.predict(responses_test)
+print(store)
+print(response_type_test)
+percent = store - response_type_test
+print(percent)
+count = 0
+for i in range(0, len(percent)):
+    if percent[i] == 0:
+        count = count + 1
+print("Точность:" + str(int(100 * count / len(percent))) + "%")
+
+
